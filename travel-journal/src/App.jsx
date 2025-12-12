e9aa907bd12206a58483d188ba09735242f0bcfc
Marcel <marwozniak01@gmail.com>
2025-12-12 13:54:40 +0100

    Passing object as props

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -4,17 +4,7 @@ import data from "./data";
 
 export default function App() {
   const entryElements = data.map((entry) => {
-    return (
-      <Entry
-        key={entry.id}
-        img={entry.img}
-        title={entry.title}
-        country={entry.country}
-        googleMapsLink={entry.googleMapsLink}
-        dates={entry.dates}
-        text={entry.text}
-      />
-    );
+    return <Entry key={entry.id} entry={entry} />;
   });
 
   return (
